995c2859f4913daa83155fd73a8f0fec53675e2d
Victor Manuel <mbvictorsantos@gmail.com>
2023-10-13 22:52:41 -0300

    feat: Initial version

---
+++ code/topic2/another_code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+// n acho que eu seja bom, mas eu ja fui pior
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "Topic 2 file" << '\n';
+    
+    return 0;
+}
